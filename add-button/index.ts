6854457b2f232fd7677ebd4e3996d2b0b0e86016
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2022-11-20 14:37:24 +0200

    Added example with local package for sharing code

--- add-button/index.ts
+++ add-button/index.ts
@@ -4,15 +4,20 @@ import type { Config } from "./config";
 
 import slug from "slug";
 
+import * as myMath from "my-math";
+
 export const main = async (
-  params: Config,
-  context: vscode.ExtensionContext
+    params: Config,
+    context: vscode.ExtensionContext
 ): Promise<ScriptRunResult> => {
+    console.log("hello darkness " + params.tooltip);
+    vscode.window.showInformationMessage(slug("Hello, Script Plus!"));
 
-  console.log("hello darkness " + params.tooltip);
-  vscode.window.showInformationMessage(slug("Hello, Script Plus!"));
+    console.log(myMath.add(1, 4));
+    console.log(myMath.add(7, 4));
+    console.log(myMath);
 
-  return () => {
-    vscode.window.showInformationMessage("Cleanup");
-  }
+    return () => {
+        vscode.window.showInformationMessage("Cleanup");
+    };
 };
---
+++ local-packages/my-math/my-math.ts
@@ -0,0 +1,11 @@
+import slug from "slug";
+
+export function add(a: number, b: number) {
+    return a + b;
+}
+
+export function subtract(a: number, b: number) {
+    return a - b;
+}
+
+slug("hello there");
